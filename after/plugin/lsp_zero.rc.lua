1abf0162ac65677b07e38f7fd8a02d31bb20a1ce
Wissam Youssef <wissam@users.noreply.github.com>
2023-04-04 03:50:09 -0400

    Added some gopls settings for lsp_zero

--- after/plugin/lsp_zero.rc.lua
+++ after/plugin/lsp_zero.rc.lua
@@ -1,10 +1,29 @@
 -- same as plugin = require('lsp-zero') in a try/catch block in other langs
 local status, plugin = pcall(require, 'lsp-zero')
 if not status then
-  print('Something went wrong:', plugin)
+    print('Something went wrong:', plugin)
 else
-  plugin.preset('recommended')
-  plugin.setup()
+    plugin.preset('recommended')
+    plugin.on_attach(function(client, bufnr)
+        plugin.default_keymaps({buffer = bufnr})
+    end)
+    require('lspconfig').gopls.setup(plugin.setup({
+        cmd = {'gopls'},
+        filetypes = {'go', 'gomod','gowork','gotmpl'},
+        root_dir = plugin.root_pattern('go.mod', '.git'),
+        single_file_support = true,
+        settings = {
+            gopls = {
+                analyses = {
+                    unusedparams = true,
+                },
+                staticcheck = true,
+                gofumpt = true,
+            },
+        },
+    }))
+
+    plugin.setup()
     vim.diagnostic.config({
         virtual_text = true,
         signs = true,
