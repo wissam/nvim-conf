c3722e94558fb5b6661e67097b58b9d2206c69a2
Wissam Youssef <wissam@users.noreply.github.com>
2023-05-17 21:12:17 -0400

    oops forgot surround conf

---
+++ after/plugin/surround.rc.lua
@@ -0,0 +1,7 @@
+-- same as plugin = require('plugin_name') in a try/catch block in other langs
+local status, plugin = pcall(require, 'nvim-surround')
+if not status then
+  print('Something went wrong:', plugin)
+else
+  plugin.setup()
+end
